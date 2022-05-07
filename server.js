59d9174786efad55ad4e5038372423489ccb79df
bnasholm <breannanasholm@gmail.com>
2022-05-07 11:36:15 -0700

    server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const express = require("express");
-const publicPath = path.join(__dirname, '.', 'public');
+const publicPath = path.join(__dirname, '.', 'build');
 const PORT = process.env.PORT || 3001;
 const axios = require("axios");
 const app = express();
