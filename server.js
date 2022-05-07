e996ca9ab1240d4dbc09c1502ac498394507492d
bnasholm <breannanasholm@gmail.com>
2022-05-07 11:14:14 -0700

    updating server.js

--- server.js
+++ server.js
@@ -4,9 +4,16 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 
-app.get('/', async (req, res) => {
-    res.status(200).send("hello???")  
-}); 
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+  
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 app.get("/api/waittimes/:id", async (req, res) => {
   const parkId = req.params.id;
